5b7e20dd508267850e81d613f16958930a633bb8
elliotgedwards <elliotgedwards@gmail.com>
2025-02-04 19:11:51 -0500

    Update lab2_q4.py

--- lab2_q4.py
+++ lab2_q4.py
@@ -9,26 +9,26 @@ df = pd.read_csv('q3_answer.csv')
 
 df['est_stop_distance'] = round((df['reaction_time'] * df['speed']) + df['speed'] ** 2 / (2 * 9.8 * (df['slope'] + df['road_type_coe']) * df['tire_type_coe']), 2)
 
-# distance_compare = []
+distance_compare = []
 
-# for index, row in df.iterrows():
-#     if row['est_stop_distance'] == row['max_stop_distance']:
-#         distance_compare.append('equal')
-#     elif row['est_stop_distance'] > row['max_stop_distance']:
-#         distance_compare.append('shorter')
-#     else:
-#         distance_compare.append('longer')
+for index, row in df.iterrows():
+    if row['est_stop_distance'] == row['stop_distance']:
+        distance_compare.append('equal')
+    elif row['est_stop_distance'] > row['stop_distance']:
+        distance_compare.append('shorter')
+    else:
+        distance_compare.append('longer')
 
-# df['distance_compare'] = distance_compare
-def compare(est, actual):
-	if est == actual: return 'equal'
-	if est > actual: return 'shorter'
-	return 'longer'
+df['distance_compare'] = distance_compare
+# def compare(est, actual):
+# 	if est == actual: return 'equal'
+# 	if est > actual: return 'shorter'
+# 	return 'longer'
 
-df["distance_compare"] = (df[["est_stop_distance", "stop_distance"]].apply(lambda x: compare(x["est_stop_distance"], x["stop_distance"]), axis=1))
+# df["distance_compare"] = (df[["est_stop_distance", "stop_distance"]].apply(lambda x: compare(x["est_stop_distance"], x["stop_distance"]), axis=1))
 
-# corr = pd.read_csv('q4_answer.csv')
+corr = pd.read_csv('q4_answer.csv')
 
-# print(df == corr)
+print(df == corr)
 
 df.to_csv(path_or_buf="./q4_answer.csv", index= False)
